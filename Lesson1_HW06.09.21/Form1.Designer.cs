ead2a838afe5eaf5001cab86232b6cde420583dd
Bohdan <dan.pastukh@gmail.com>
2021-10-01 16:46:49 +0300

    Home work commit

---
+++ Lesson1_HW06.09.21/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace Lesson1_HW06._09._21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxStart = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numericUpDownStart = new System.Windows.Forms.NumericUpDown();
+            this.textBoxEnd = new System.Windows.Forms.TextBox();
+            this.numericUpDownEnd = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDownPortStart = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownPortEnd = new System.Windows.Forms.NumericUpDown();
+            this.buttonScan = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEnd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPortStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPortEnd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxStart
+            // 
+            this.textBoxStart.Location = new System.Drawing.Point(273, 11);
+            this.textBoxStart.Name = "textBoxStart";
+            this.textBoxStart.Size = new System.Drawing.Size(59, 20);
+            this.textBoxStart.TabIndex = 0;
+            this.textBoxStart.Text = "192.168.0.";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(198, 342);
+            this.listBox1.TabIndex = 1;
+            // 
+            // numericUpDownStart
+            // 
+            this.numericUpDownStart.Location = new System.Drawing.Point(338, 12);
+            this.numericUpDownStart.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownStart.Name = "numericUpDownStart";
+            this.numericUpDownStart.Size = new System.Drawing.Size(39, 20);
+            this.numericUpDownStart.TabIndex = 2;
+            // 
+            // textBoxEnd
+            // 
+            this.textBoxEnd.Location = new System.Drawing.Point(273, 43);
+            this.textBoxEnd.Name = "textBoxEnd";
+            this.textBoxEnd.Size = new System.Drawing.Size(59, 20);
+            this.textBoxEnd.TabIndex = 3;
+            this.textBoxEnd.Text = "192.168.0.";
+            // 
+            // numericUpDownEnd
+            // 
+            this.numericUpDownEnd.Location = new System.Drawing.Point(338, 43);
+            this.numericUpDownEnd.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownEnd.Name = "numericUpDownEnd";
+            this.numericUpDownEnd.Size = new System.Drawing.Size(39, 20);
+            this.numericUpDownEnd.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IPStart";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "IPEnd";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "PortStart";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(216, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "PortEnd";
+            // 
+            // numericUpDownPortStart
+            // 
+            this.numericUpDownPortStart.Location = new System.Drawing.Point(273, 71);
+            this.numericUpDownPortStart.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownPortStart.Name = "numericUpDownPortStart";
+            this.numericUpDownPortStart.Size = new System.Drawing.Size(104, 20);
+            this.numericUpDownPortStart.TabIndex = 9;
+            // 
+            // numericUpDownPortEnd
+            // 
+            this.numericUpDownPortEnd.Location = new System.Drawing.Point(273, 104);
+            this.numericUpDownPortEnd.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownPortEnd.Name = "numericUpDownPortEnd";
+            this.numericUpDownPortEnd.Size = new System.Drawing.Size(104, 20);
+            this.numericUpDownPortEnd.TabIndex = 10;
+            // 
+            // buttonScan
+            // 
+            this.buttonScan.Location = new System.Drawing.Point(219, 130);
+            this.buttonScan.Name = "buttonScan";
+            this.buttonScan.Size = new System.Drawing.Size(158, 73);
+            this.buttonScan.TabIndex = 11;
+            this.buttonScan.Text = "Scan Ports";
+            this.buttonScan.UseVisualStyleBackColor = true;
+            this.buttonScan.Click += new System.EventHandler(this.buttonScan_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 357);
+            this.Controls.Add(this.buttonScan);
+            this.Controls.Add(this.numericUpDownPortEnd);
+            this.Controls.Add(this.numericUpDownPortStart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownEnd);
+            this.Controls.Add(this.textBoxEnd);
+            this.Controls.Add(this.numericUpDownStart);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBoxStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEnd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPortStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPortEnd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxStart;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDownStart;
+        private System.Windows.Forms.TextBox textBoxEnd;
+        private System.Windows.Forms.NumericUpDown numericUpDownEnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDownPortStart;
+        private System.Windows.Forms.NumericUpDown numericUpDownPortEnd;
+        private System.Windows.Forms.Button buttonScan;
+    }
+}
+
---
+++ Lesson1_HW06.09.21/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson1_HW06._09._21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonScan_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Socket socket = null;
+            int portStart = (int)numericUpDownPortStart.Value;
+            int portEnd = (int)numericUpDownPortEnd.Value;
+
+            Task.Factory.StartNew(() => {
+                Parallel.For((int)numericUpDownStart.Value,(int) numericUpDownEnd.Value+1, iPcount =>
+                 {
+                     IPAddress iP= IPAddress.Parse($"{textBoxStart.Text}{iPcount}");
+                     Parallel.For(portStart, portEnd, port => {
+                         try
+                         {
+                             //Створюєм кінцеву точку
+                             IPEndPoint iPEndPoint = new IPEndPoint(iP, port);//ip adress+номер порта/ 
+
+                             //Створення сокета
+                             socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//AddressFamily.InterNetwork - IPv4 
+
+                             socket.Connect(iPEndPoint);
+
+                             listBox1.Invoke(new Action(() => listBox1.Items.Add($"IP {iP} | Port is busy: {port}")));
+                         }
+                         catch (Exception ex)
+                         {
+                             listBox1.Invoke(new Action(() => listBox1.Items.Add($"IP {iP} | Port is free: {port}")));
+                         }
+                         finally
+                         {
+                             socket.Close();
+                         }
+                     });
+                 });
+            });
+                
+        }
+    }
+}
